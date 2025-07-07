cc9c60ce7322f69d0b4a65d337b230e87ca21d8b
Rushikesh9706 <rushikeshmusale301@gmail.com>
2025-07-07 16:33:25 +0530

    Add files via upload

---
+++ backend/api/bookings.js
@@ -0,0 +1,3 @@
+// API route for bookings (proxy to routes/bookings)
+const bookingsRoutes = require('../routes/bookings');
+module.exports = bookingsRoutes;
---
+++ backend/api/wallet.js
@@ -0,0 +1,3 @@
+// API route for wallet (proxy to routes/wallet)
+const walletRoutes = require('../routes/wallet');
+module.exports = walletRoutes;
